db7810caf679071c4065540361966a37334fce27
zehradenizli4 <zehradenizli4@gmail.com>
2026-06-15 19:41:26 +0300

    Proje tanıtımı bileşeni kodlandı

---
+++ src/components/ProjectIntro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ProjectIntro() {
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '15px', margin: '10px 0', borderRadius: '8px' }}>
+      <h3>📢 Proje Tanıtımı</h3>
+      <p>MahalleNabzı nedir? Amacımız, vizyonumuz ve değerlerimiz...</p>
+    </div>
+  )
+}
+
+export default ProjectIntro
