023f0d712242fd1580b1455bd2d4e383be9c3f79
zehradenizli4 <zehradenizli4@gmail.com>
2026-06-15 19:54:46 +0300

    App ana bilesenine guvenlik ve gizlilik sayfasi baglandi

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import AnalysisMap from './pages/AnalysisMap'
 import ImpactSuccess from './pages/ImpactSuccess'
-import UserActions from './pages/UserActions' // Yeni ekledik
+import UserActions from './pages/UserActions'
+import PrivacySecurity from './pages/PrivacySecurity' // Yeni ekledik
 
 function App() {
   return (
@@ -19,8 +20,11 @@ function App() {
       <ImpactSuccess />
       <hr style={{ margin: '40px 0', border: '0', borderTop: '1px solid #ccc' }} />
       
-      {/* Yeni eklediğimiz modül */}
       <UserActions />
+      <hr style={{ margin: '40px 0', border: '0', borderTop: '1px solid #ccc' }} />
+      
+      {/* Yeni eklediğimiz modül */}
+      <PrivacySecurity />
       
       <footer style={{ textAlign: 'center', padding: '20px', background: '#f1f1f1', marginTop: '40px' }}>
         <p>© 2026 MahalleNabzı Dijital Dayanışma Platformu</p>
