e42dcccd4e2086e3c30e07ad6cf7937216d3f8ac
zehradenizli4 <zehradenizli4@gmail.com>
2026-06-15 19:53:11 +0300

    App ana bilesenine kullanici islemleri sayfasi baglandi

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import AnalysisMap from './pages/AnalysisMap'
-import ImpactSuccess from './pages/ImpactSuccess' // Yeni ekledik
+import ImpactSuccess from './pages/ImpactSuccess'
+import UserActions from './pages/UserActions' // Yeni ekledik
 
 function App() {
   return (
@@ -15,8 +16,11 @@ function App() {
       <AnalysisMap />
       <hr style={{ margin: '40px 0', border: '0', borderTop: '1px solid #ccc' }} />
       
-      {/* Yeni eklediğimiz modül */}
       <ImpactSuccess />
+      <hr style={{ margin: '40px 0', border: '0', borderTop: '1px solid #ccc' }} />
+      
+      {/* Yeni eklediğimiz modül */}
+      <UserActions />
       
       <footer style={{ textAlign: 'center', padding: '20px', background: '#f1f1f1', marginTop: '40px' }}>
         <p>© 2026 MahalleNabzı Dijital Dayanışma Platformu</p>
